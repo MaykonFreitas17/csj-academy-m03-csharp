b787fa3239fccdd4d07274cb68ade6bdaad580de
Maykon Freitas <maykon17programmer@gmail.com>
2022-11-25 17:04:17 -0300

    Feat: Adiciona o Script FirstScritp.cs
    Adiciona primeiro arquivo .cs (csharp), arquivo de teste.

---
+++ csj-academy-m03-csharp/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,20 @@
+// Bibliotecas da Propria Unity
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Classe Principal do Script Criado - A classe que herda da classe da Unity MonoBehaviour
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
